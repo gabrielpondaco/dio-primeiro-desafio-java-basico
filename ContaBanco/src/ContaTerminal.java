4c5014d6b88068112e765b7f64be55574c0d50de
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2024-04-29 09:02:17 -0300

    feat: add new Java Project for ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,14 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        /*TODO: Conhecer e Importar a classe Scanner
+        Exibir as mensagens para o usuário
+        Obter os valores digitados no terminal
+        Exibit a mensagem conta criada
+        Atributo 	Tipo 	Exemplo
+        Numero 	Inteiro 	1021    -> Int
+        Agencia 	Texto 	067-8   -> String
+        Nome Cliente 	Texto 	MARIO ANDRADE -> String
+        Saldo 	Decimal 	237.48  -> Double
+        */ 
+    }
+}
